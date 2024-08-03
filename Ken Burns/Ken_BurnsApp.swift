33af1366f340d7a4402f5bd09d84029006a254d4
AdamEXu <contact@adamxu.net>
2024-08-03 13:29:28 -0700

    Initial Commit

---
+++ Ken Burns/Ken_BurnsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ken_BurnsApp.swift
+//  Ken Burns
+//
+//  Created by Adam Xu on 8/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Ken_BurnsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ken BurnsUITests/Ken_BurnsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Ken_BurnsUITestsLaunchTests.swift
+//  Ken BurnsUITests
+//
+//  Created by Adam Xu on 8/3/24.
+//
+
+import XCTest
+
+final class Ken_BurnsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
